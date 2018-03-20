086857e0031d8171e39cf059a91df4dae523617b
SunnyShikhar <shikhar.gaur.13@gmail.com>
2018-03-19 23:20:03 -0400

    removed timezone

--- capstone/settings.py
+++ capstone/settings.py
@@ -46,7 +46,6 @@ CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = TIME_ZONE
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
