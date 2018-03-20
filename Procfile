6a8323da330331114c5d40682b6161dc9ee31637
SunnyShikhar <shikhar.gaur.13@gmail.com>
2018-03-19 23:17:42 -0400

    added celery + redis to Django

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn capstone.wsgi --log-file -
+worker: celery -A capstone worker
--- capstone/settings.py
+++ capstone/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CELERY_BROKER_URL = os.environ.get('REDIS_URL')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL')
+
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
