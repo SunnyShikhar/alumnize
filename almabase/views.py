fc424c084b67099a580693f227ebac768eda3229
SunnyShikhar <shikhar.gaur.13@gmail.com>
2018-03-20 04:37:33 -0400

    removed post check

--- almabase/views.py
+++ almabase/views.py
@@ -21,21 +21,32 @@ def loading(request):
     return render(request, "loading.html")
 
 def upload(request):
-    if request.POST and request.FILES:
-        print(request)
-        print(request.POST)
-        print(request.FILES)
-        print(request.FILES.getlist("files"))
-        print(request.FILES.getlist("master"))
-        
-        almaInput = []
-        
-        for eachFile in request.FILES.getlist("files"):
-            almaInput.append(pd.read_csv(eachFile))
 
-        masterFile = [pd.read_csv(request.FILES.getlist("master")[0])]
-        request.close()
+    almaInput = []
 
-        return beginTransform(almaInput, masterFile)
-    else:
-        return render(request,"error.html")         
+    for eachFile in request.FILES.getlist("files"):
+        almaInput.append(pd.read_csv(eachFile))
+
+    masterFile = [pd.read_csv(request.FILES.getlist("master")[0])]
+    request.close()
+
+    return beginTransform(almaInput, masterFile)
+
+    # if request.POST and request.FILES:
+    #     print(request)
+    #     print(request.POST)
+    #     print(request.FILES)
+    #     print(request.FILES.getlist("files"))
+    #     print(request.FILES.getlist("master"))
+        
+    #     almaInput = []
+        
+    #     for eachFile in request.FILES.getlist("files"):
+    #         almaInput.append(pd.read_csv(eachFile))
+
+    #     masterFile = [pd.read_csv(request.FILES.getlist("master")[0])]
+    #     request.close()
+
+    #     return beginTransform(almaInput, masterFile)
+    # else:
+    #     return render(request,"error.html")         
